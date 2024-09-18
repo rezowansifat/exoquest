2d1c7eeb894ec18b4582839b8da99a179df235b5
rezowansifat <navidrezowansifat@gmail.com>
2024-09-19 00:50:17 +0600

    Exomap init

--- src/pages/Game/ExoMap.jsx
+++ src/pages/Game/ExoMap.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import ExoDrawer from "../../components/ExoDrawer";
+import mapbg from "../../assets/pages/bg/mapbg.png";
+
+import {
+  kepler186f,
+  kepler22b,
+  Gliese,
+  trappist1e,
+  kepler442b,
+  kepler62f,
+  Ellipse,
+} from "../../assets/games/ExoPlanets";
 
 const ExoMap = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const toggleDrawer = (open) => {
+    setDrawerOpen(open);
+  };
+
   return (
     <Main>
-      <Container>hello</Container>
+      <Container>
+        <BackArrow onClick={handleBack}>
+          <ArrowBackIcon />
+        </BackArrow>
+        <MenuBar onClick={() => toggleDrawer(true)}>
+          <MenuIcon />
+        </MenuBar>
+        <ExoDrawer open={drawerOpen} toggleDrawer={toggleDrawer} />
+
+        {/* map */}
+      </Container>
     </Main>
   );
 };
@@ -15,6 +51,7 @@ const Main = styled.div`
   background-size: cover;
   background-position: center;
   position: relative;
+  background-image: url(${mapbg});
 `;
 
 const Container = styled.div`
@@ -22,10 +59,37 @@ const Container = styled.div`
   height: 100%;
   margin: 0 auto;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   position: relative;
   z-index: 2;
+  overflow: hidden;
+`;
+
+const BackArrow = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 0;
+  z-index: 100;
+  svg {
+    font-size: 40px;
+    font-weight: 800;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
+
+const MenuBar = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 0;
+  z-index: 100;
+  svg {
+    font-size: 40px;
+    font-weight: 800;
+    color: #fff;
+    cursor: pointer;
+  }
 `;
 
 export default ExoMap;
